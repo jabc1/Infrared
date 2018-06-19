0a2981ebf45ab601fbb62bdfed341bf7704c9955
kk <38032566+jabc1@users.noreply.github.com>
2018-06-19 10:42:47 +0800

    Update jisuan.c

--- jisuan.c
+++ jisuan.c
@@ -2,15 +2,22 @@
 #define T	1000000//us
 main()
 {
-	int temp = 0;
+	int temp = 0,i;
 	float use_t = 0;
 	printf("please input HZ?\r\n");
 	scanf("%d",&temp);
 	//printf("temp=%d",temp);
+	if(temp > 1000)
+	{
+		temp = temp / 1000;
+	}
 	use_t = (float)(T / (float)temp);
+	printf("input HZ = %dKHz\r\n",temp);
 	printf("use_t=%fus\tT=%fus\r\n",use_t,use_t/2);
+	i = (int)(use_t/2);
+	printf("htimx.Init.Prescaler=72-1\r\n");
+	printf("htimx.Init.Prescaler=%d-1\r\n",i);
 }
-
 
 
 
